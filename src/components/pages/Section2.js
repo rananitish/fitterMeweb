056118df935b26530a1bddeac1a2f38f773a8ab8
Nitish Rana <nitishrana@Nitishs-MacBook-Air.local>
2020-12-30 21:33:57 +0530

    new section is created

--- src/components/pages/Section2.js
+++ src/components/pages/Section2.js
@@ -3,10 +3,13 @@ import "./Section2.css"
 
 function Section2() {
     return (
-        <div>
+        <div className='container'>
+        
         <h2 className="heading">
         Here's how we work</h2>
-            
+        <div className="nav--bar"><li>nitish</li><li>hello</li>
+        </div>
+        
         </div>
     )
 }
